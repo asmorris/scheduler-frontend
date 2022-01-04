be11e82babf5d6e524d4cf92f43576816396078a
Andrew Morrison <morro141@gmail.com>
2022-01-04 16:41:39 -0500

    initial commit, took too long whoops.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+	return (
+		<button
+			className={classNames("button", {
+				"button--confirm": props.confirm,
+				"button--danger": props.danger,
+			})}
+			disabled={props.disabled}
+			onClick={props.disabled ? null : props.onClick}
+		>
+			{props.children}
+		</button>
+	);
 }
