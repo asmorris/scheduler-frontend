37ab0f3788f956922a3d01dcdee5e1a83edd8992
Andrew Morrison <morro141@gmail.com>
2022-01-04 16:48:43 -0500

    update interviewer list

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 
 import "./InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
 	return (
-		<li className="interviewers__item">
+		<li
+			onClick={() => props.setInterviewer(props.name)}
+			className={classNames("interviewers__item", {
+				"interviewers__item--selected": props.selected,
+			})}
+		>
 			<img
 				className="interviewers__item-image"
-				src="https://i.imgur.com/LpaY82x.png"
-				alt="Sylvia Palmer"
+				src={props.avatar}
+				alt={props.name}
 			/>
-			Sylvia Palmer
+			{props.selected && props.name}
 		</li>
 	);
 }
