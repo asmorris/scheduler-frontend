df8971b958ceebbad64ab8fb614148cebb0aa24f
Andrew Morrison <morro141@gmail.com>
2022-01-04 20:53:37 -0500

    add in emtpy and header

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 export default function Appointment() {
-	return <div></div>;
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
 	.addParameters({
@@ -141,4 +143,6 @@ storiesOf("Appointment", module)
 		backgrounds: [{ name: "white", value: "#fff", default: true }],
 	})
 	.add("Appointment", () => <Appointment />)
-	.add("With Time", () => <Appointment time="12pm" />);
+	.add("With Time", () => <Appointment time="12pm" />)
+	.add("Header", () => <Header time="12pm" />)
+	.add("Empty", () => <Empty onAdd={action("onAdd")} />);
